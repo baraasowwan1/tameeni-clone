e5eb25ca9dd390414ca205bded9fa140ca15e6b6
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-01-04 21:44:24 +0300

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
